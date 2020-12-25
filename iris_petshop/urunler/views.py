c8616fd8dd12b6c9db54f44c88dd7dd83435cc18
iremkilikya <iremkilikya@gmail.com>
2020-12-25 17:29:31 +0300

    feat: index.html çağrıldı

--- iris_petshop/urunler/views.py
+++ iris_petshop/urunler/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def list_products(request):
+    html = "<p>Ürünler</p>"
+    return HttpResponse(html)
